da3dae2aa53b4cb1e524fe0b3afdc481d3ae98dc
Sumankanta Padhan <sumankpadhan@gmail.com>
2025-06-18 08:51:26 +0530

    Icon Added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -179,7 +179,7 @@ const Login = () => {
                     <div className="mt-4 grid space-y-4">
                         <button onClick={handleRegister} className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
                             <div className="relative flex items-center space-x-4 justify-center">
-                                <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" className="absolute left-0 w-5" alt="google logo" />
+                                <img src="https://img.icons8.com/?size=100&id=V5cGWnc9R4xj&format=png&color=000000" className="absolute left-0 w-5" alt="google logo" />
                                 <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">Continue with Google</span>
                             </div>
                         </button>
